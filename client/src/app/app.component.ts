290b94c8094208fd170f7b5ad3f1800a17a7b674
unknown <javoxiraka@gmail.com>
2020-01-06 02:36:59 +0500

    fix selected nikud draft view

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -100,7 +100,7 @@ export class AppComponent implements OnInit, OnDestroy {
   private selectNikud(nikud){
     console.log(nikud)
     this.undoWord = ''+nikud
-    this.selectedNikuds = splitWholeNikuds(nikud)
+    this.selectedNikuds = splitWholeNikuds(nikud).reverse()
   }
  
   private selectLetter(index:number){
